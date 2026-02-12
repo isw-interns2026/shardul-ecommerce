1057a8a699150ac68721e56796a4154a92ac4751
Shardul Deshpande <Shardul.Deshpande@insightsoftware.com>
2026-02-12 11:19:48 +0530

    Migration for StripeSessionId in Transaction

---
+++ eCommerce/Migrations/20260212054900_AddStripeSessionIdToTransaction.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStripeSessionIdToTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StripeSessionId",
+                table: "Transactions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_StripeSessionId",
+                table: "Transactions",
+                column: "StripeSessionId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_StripeSessionId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "StripeSessionId",
+                table: "Transactions");
+        }
+    }
+}
